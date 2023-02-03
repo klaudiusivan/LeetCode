b9556d93b01b7f11b84ee62b11fea758e960d531
Klaudius ivan <klaudiusivanr>
2023-02-03 22:07:37 +0700

    implement binary search while loop with test searchInsert should return the expected index when the target is available in the nuts

--- SearchInsertPositionTests/SearchInsertPositionTests.swift
+++ SearchInsertPositionTests/SearchInsertPositionTests.swift
@@ -9,7 +9,22 @@ import XCTest
 
 class Solution {
     func searchInsert(_ nums: [Int], _ target: Int) -> Int {
-        return 0
+        var left = 0
+        var right = nums.count - 1
+        
+        var expectedIndex = 0
+        while left < right {
+            expectedIndex = (left+right) / 2
+            if nums[expectedIndex] == target {
+                return expectedIndex
+            } else if nums[expectedIndex] > target {
+                right = expectedIndex - 1
+            } else {
+                left = expectedIndex + 1
+            }
+        }
+        
+        return expectedIndex
     }
 }
 
@@ -25,5 +40,13 @@ final class SearchInsertPositionTests: XCTestCase {
         let result = sut.searchInsert([], 4)
         
         XCTAssertEqual(result, 0)
+    }
+    
+    func test_searchInsert_shouldReturnTwoOnExistTargetNumberInNums() {
+        let sut = Solution()
+        
+        let result = sut.searchInsert([1,3,5,6], 5)
+        
+        XCTAssertEqual(result, 2)
     }
 }
