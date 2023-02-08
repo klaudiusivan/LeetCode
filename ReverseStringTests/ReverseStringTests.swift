21d66f70f4513bc7d93001782c2272139d59275a
Klaudius ivan <klaudiusivanr>
2023-02-08 20:37:42 +0700

    add reverse string setup

---
+++ ReverseStringTests/ReverseStringTests.swift
@@ -0,0 +1,35 @@
+//
+//  ReverseStringTests.swift
+//  ReverseStringTests
+//
+//  Created by Klaudius Ivan Anteraja on 08/02/23.
+//
+
+import XCTest
+
+class Solution {
+    func reverseString(_ s: inout [Character]) {
+        
+    }
+}
+
+/**
+ Write a function that reverses a string. The input string is given as an array of characters s.
+
+ You must do this by modifying the input array in-place with O(1) extra memory.
+
+ ~~~
+ Example 1:
+
+ Input: s = ["h","e","l","l","o"]
+ Output: ["o","l","l","e","h"]
+ Example 2:
+
+ Input: s = ["H","a","n","n","a","h"]
+ Output: ["h","a","n","n","a","H"]
+ ~~~
+ */
+final class ReverseStringTests: XCTestCase {
+
+    
+}
