567e7c76a92e888567eb21890aad5513c994d319
Klaudius ivan <klaudiusivanr>
2023-03-10 20:56:01 +0700

    add case Permutation

---
+++ PermutationTests/PermutationTests.swift
@@ -0,0 +1,40 @@
+//
+//  PermutationTests.swift
+//  PermutationTests
+//
+//  Created by Klaudius Ivan Anteraja on 10/03/23.
+//
+
+import XCTest
+
+class Solution {
+    func permute(_ nums: [Int]) -> [[Int]] {
+        
+    }
+}
+
+/**
+ Given an array nums of distinct integers, return all the possible permutations. You can return the answer in any order.
+
+ Example 1:
+
+ Input: nums = [1,2,3]
+ Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+ Example 2:
+
+ Input: nums = [0,1]
+ Output: [[0,1],[1,0]]
+ Example 3:
+
+ Input: nums = [1]
+ Output: [[1]]
+
+ Constraints:
+
+ 1 <= nums.length <= 6
+ -10 <= nums[i] <= 10
+ All the integers of nums are unique.
+ */
+final class PermutationTests: XCTestCase {
+
+}
